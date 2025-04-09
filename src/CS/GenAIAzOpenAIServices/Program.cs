1e82d08e1f7c2b62154847bf91192f121d461245
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-09 23:08:38 +0530

    Changes

--- src/CS/GenAIAzOpenAIServices/Program.cs
+++ src/CS/GenAIAzOpenAIServices/Program.cs
@@ -1,10 +1,5 @@
 ﻿// Implicit using statements are included
-using System.Text;
-using System.ClientModel;
-using System.Text.Json;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using Azure;
 
 // Add Azure OpenAI packages
 
