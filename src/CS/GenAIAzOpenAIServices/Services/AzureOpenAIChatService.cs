891c6a6264bc83b39a758715540466312dbd2413
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-11 15:10:14 +0530

    Changes

---
+++ src/CS/GenAIAzOpenAIServices/Services/AzureOpenAIChatService.cs
@@ -0,0 +1,13 @@
+﻿using AzAIServicesCommon.Configuration;
+
+namespace GenAIAzOpenAIServices.Services;
+
+#pragma warning disable CA1303
+
+internal sealed class AzureOpenAIChatService
+{
+    public static async Task ShowChatCompletionDemowithAzAOI(AzAISvcAppConfiguration appConfig)
+    {
+
+    }
+}
