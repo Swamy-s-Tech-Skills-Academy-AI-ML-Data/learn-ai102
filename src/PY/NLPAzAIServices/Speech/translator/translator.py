dafbd954d7b971abc68a0b3874f03e936c44cdc7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-28 15:45:33 +0530

    Update translator.py

--- src/PY/NLPAzAIServices/Speech/translator/translator.py
+++ src/PY/NLPAzAIServices/Speech/translator/translator.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 from datetime import datetime
 import os
-
 # Import namespaces
+import azure.cognitiveservices.speech as speech_sdk
 
 
 def main():
@@ -23,32 +23,46 @@ def main():
         ai_region = os.getenv('SPEECH_REGION')
 
         # Configure translation
-
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(
+            ai_key, ai_region)
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('hi')
+        print('Ready to translate from',
+              translation_config.speech_recognition_language)
 
         # Configure speech
-
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
 
         # Get user input
         targetLanguage = ''
         while targetLanguage != 'quit':
-            targetLanguage = input('\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n').lower()
+            targetLanguage = input(
+                '\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n').lower()
             if targetLanguage in translation_config.target_languages:
                 Translate(targetLanguage)
             else:
                 targetLanguage = 'quit'
-                
 
     except Exception as ex:
         print(ex)
 
+
 def Translate(targetLanguage):
     translation = ''
 
-    # Translate speech
-
+    # Translate speech using the default microphone
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    translator = speech_sdk.translation.TranslationRecognizer(
+        translation_config, audio_config=audio_config)
+    print("Speak now...")
+    result = translator.recognize_once_async().get()
+    print('Translating "{}"'.format(result.text))
+    translation = result.translations[targetLanguage]
+    print(translation)
 
     # Synthesize translation
-
 
 
 if __name__ == "__main__":
