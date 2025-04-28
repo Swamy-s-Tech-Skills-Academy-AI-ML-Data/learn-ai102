e3ca0a210b37608ecd9d5f0062158ff5cd008ed7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-28 14:47:15 +0530

    Changes

---
+++ src/CS/NLPAzAIServices/Services/TranslateSpeechWithAzureAIService.cs
@@ -0,0 +1,108 @@
+﻿using AzAIServicesCommon.Configuration;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
+using System.Media;
+using System.Text;
+
+namespace NLPAzAIServices.Services;
+
+internal sealed class TranslateSpeechWithAzureAIService
+{
+    public static async Task ShowTranslateSpeechDemoWithAzureAIService(AzAISvcAppConfiguration appConfig)
+    {
+        SpeechConfig speechConfig;
+        SpeechTranslationConfig translationConfig;
+
+        if (string.IsNullOrEmpty(appConfig?.NLPAzAIServices?.SpeechAIService?.Endpoint) ||
+            string.IsNullOrEmpty(appConfig?.NLPAzAIServices?.SpeechAIService?.Key) ||
+            string.IsNullOrEmpty(appConfig?.NLPAzAIServices?.SpeechAIService?.Region))
+        {
+            WriteLine("Please check your appsettings.json file for missing or incorrect values.");
+            return;
+        }
+
+        try
+        {
+            // Get config settings from AppSettings
+            string aiSvcKey = appConfig?.NLPAzAIServices?.SpeechAIService?.Key!;
+            string aiSvcRegion = appConfig?.NLPAzAIServices?.SpeechAIService?.Region!;
+
+            // Configure translation
+            translationConfig = SpeechTranslationConfig.FromSubscription(aiSvcKey, aiSvcRegion);
+            translationConfig.SpeechRecognitionLanguage = "en-US";
+            translationConfig.AddTargetLanguage("fr");
+            translationConfig.AddTargetLanguage("es");
+            translationConfig.AddTargetLanguage("hi");
+            WriteLine("Ready to translate from " + translationConfig.SpeechRecognitionLanguage);
+
+            // Configure speech
+            speechConfig = SpeechConfig.FromSubscription(aiSvcKey, aiSvcRegion);
+
+            string targetLanguage = "";
+            while (targetLanguage != "quit")
+            {
+                WriteLine("\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n");
+                targetLanguage = ReadLine()?.ToLowerInvariant()!;
+                if (translationConfig.TargetLanguages.Contains(targetLanguage))
+                {
+                    await Translate(targetLanguage);
+                }
+                else
+                {
+                    targetLanguage = "quit";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            WriteLine(ex.Message);
+        }
+
+    }
+
+    private async Task Translate(string targetLanguage)
+    {
+        string translation = "";
+
+        //// Translate speech from microphone
+        //using AudioConfig audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        //using TranslationRecognizer translator = new(translationConfig, audioConfig);
+
+        //WriteLine("Speak now...");
+        //TranslationRecognitionResult result = await translator.RecognizeOnceAsync();
+        //WriteLine($"Translating '{result.Text}'");
+        //translation = result.Translations[targetLanguage];
+        //OutputEncoding = Encoding.UTF8;
+        //WriteLine(translation);
+
+        // Translate speech from file
+        string audioFile = @"D:\STSAAIMLDT\learn-ai102\src\Data\NLP\Speech\station.wav";
+        SoundPlayer wavPlayer = new(audioFile);
+        wavPlayer.Play();
+        using AudioConfig audioConfig = AudioConfig.FromWavFileInput(audioFile);
+        using TranslationRecognizer translator = new(translationConfig, audioConfig);
+        WriteLine("Getting speech from file...");
+        TranslationRecognitionResult result = await translator.RecognizeOnceAsync();
+        WriteLine($"Translating '{result.Text}'");
+        translation = result.Translations[targetLanguage];
+        OutputEncoding = Encoding.UTF8;
+        WriteLine(translation);
+
+        // Synthesize translation
+        var voices = new Dictionary<string, string>
+        {
+            ["fr"] = "fr-FR-HenriNeural",
+            ["es"] = "es-ES-ElviraNeural",
+            ["hi"] = "hi-IN-MadhurNeural"
+        };
+        speechConfig.SpeechSynthesisVoiceName = voices[targetLanguage];
+        using SpeechSynthesizer speechSynthesizer = new(speechConfig);
+        SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(translation);
+        if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+        {
+            WriteLine(speak.Reason);
+        }
+        await Task.CompletedTask.ConfigureAwait(false);
+    }
+}
