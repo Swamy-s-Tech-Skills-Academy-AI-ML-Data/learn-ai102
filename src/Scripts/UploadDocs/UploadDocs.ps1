db677b91dedd78b45e4fbbacec9baf766c8c6463
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-05-13 14:01:02 +0530

    Update UploadDocs.ps1

--- src/Scripts/UploadDocs/UploadDocs.ps1
+++ src/Scripts/UploadDocs/UploadDocs.ps1
@@ -1,14 +1,21 @@
+Param(
+    [Parameter(Mandatory=$true)][string]$subscriptionId,
+    [Parameter(Mandatory=$true)][string]$azureStorageAccount,
+    [Parameter(Mandatory=$true)][string]$azureStorageKey,
+    [Parameter()][string]$localFolderPath = f"D:\STSAAIMLDT\learn-ai102\src\Data\KnowledgeMining\Data",
+    [Parameter()][string]$containerName    = "margies"
+)
+
 # Set values for your storage account
-$subscriptionId = "YOUR_SUBSCRIPTION_ID"
-$azureStorageAccount = "YOUR_AZURE_STORAGE_ACCOUNT_NAME"
-$azureStorageKey = "YOUR_AZURE_STORAGE_KEY"
+# $subscriptionId          = "YOUR_SUBSCRIPTION_ID"
+# $azureStorageAccount    = "YOUR_AZURE_STORAGE_ACCOUNT_NAME"
+# $azureStorageKey        = "YOUR_AZURE_STORAGE_KEY"
 
 # Set the context for the subscription
 Write-Host "Setting Azure context..."
 Set-AzContext -SubscriptionId $subscriptionId
 
 $storageContext = (New-AzStorageContext -StorageAccountName $azureStorageAccount -StorageAccountKey $azureStorageKey)
-$containerName = "margies"
 
 # Create the storage container
 Write-Host "Creating container..."
@@ -17,7 +24,7 @@ New-AzStorageContainer -Name $containerName -Context $storageContext | Out-Null
 # Upload files to the container
 Write-Host "Uploading files..."
 # Recursively get all files in the local folder
-$files = Get-ChildItem -Path "./data" -Recurse -File
+$files = Get-ChildItem -Path $localFolderPath -Recurse -File
 
 # Upload each file to the blob container
 foreach ($file in $files) {
