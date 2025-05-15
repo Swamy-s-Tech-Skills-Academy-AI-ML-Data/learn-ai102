93a23a3a3796edc07b22e5b888f03e2dda18ac9f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-05-15 10:45:28 +0530

    Update modify-search.cmd

--- src/Scripts/ModifySearch/modify-search.cmd
+++ src/Scripts/ModifySearch/modify-search.cmd
@@ -1,26 +1,29 @@
 @echo off
 
 rem Set values for your Search service
-set url=https://azai-search-dev-001.search.windows.net
-set admin_key=
+set url=https://YourSearchEndpoint.search.windows.net
+set admin_key=YourSearchAdminKey
+
+rem Define base path to JSON definitions
+set basePath=D:\STSAAIMLDT\learn-ai102\src\Data\KnowledgeMining\modify-search
 
 echo -----
 echo Updating the skillset...
-call curl -X PUT %url%/skillsets/margies-skillset?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @skillset.json
+call curl -X PUT %url%/skillsets/margies-skillset?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @"%basePath%\skillset.json"
 
 rem wait
 timeout /t 2 /nobreak
 
 echo -----
 echo Updating the index...
-call curl -X PUT %url%/indexes/margies-index?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @index.json
+call curl -X PUT %url%/indexes/margies-index?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @"%basePath%\index.json"
 
 rem wait
 timeout /t 2 /nobreak
 
 echo -----
 echo Updating the indexer...
-call curl -X PUT %url%/indexers/margies-indexer?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @indexer.json
+call curl -X PUT %url%/indexers/margies-indexer?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @"%basePath%\indexer.json"
 
 echo -----
 echo Resetting the indexer
@@ -31,5 +34,5 @@ timeout /t 5 /nobreak
 
 echo -----
 echo Rerunning the indexer
-call curl -X POST %url%/indexers/margies-indexer/run?api-version=2020-06-30 -H "Content-Type: application/json" -H "Content-Length: 0" -H "api-key: %admin_key%" 
+call curl -X POST %url%/indexers/margies-indexer/run?api-version=2020-06-30 -H "Content-Type: application/json" -H "Content-Length: 0" -H "api-key: %admin_key%"
 
